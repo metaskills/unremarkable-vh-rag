6cfad28e0a0cd096e7bd4871ce42d6e43e23c205
Ken Collins <ken@metaskills.net>
2024-02-22 19:32:03 +0000

    Helper functions

--- src/demo-csv.js
+++ src/demo-csv.js
@@ -6,52 +6,21 @@ const openai = new OpenAI();
 
 let luxuryAssistant, luxuryFile, messages;
 
-// Assistant
+// Helpers
 
-const assistantsPage = await openai.beta.assistants.list({ limit: "100" });
-
-luxuryAssistant = assistantsPage.data.find((a) => a.name === "Luxury Apparel");
-
-if (luxuryAssistant !== undefined) {
-  console.log("🪲  Deleting assistant:", luxuryAssistant.id);
-  await openai.beta.assistants.del(luxuryAssistant.id);
+function log(message) {
+  console.log(`💬 ${message}`);
 }
 
-// File
-
-const fileObjectsPage = await openai.files.list();
-
-luxuryFile = fileObjectsPage.data.find(
-  (f) => f.filename === "Luxury_Products_Apparel_Data.csv"
-);
-
-if (luxuryFile !== undefined) {
-  console.log("🪲  Deleting file:", luxuryFile.id);
-  await openai.files.del(luxuryFile.id);
+function ai(message) {
+  console.log(`🤖 ${message}`);
 }
 
-// Assistant w/File, Thread, Message
-
-console.log("🪲  Creating file...");
-luxuryFile = await openai.files.create({
-  file: fs.createReadStream("data/Luxury_Products_Apparel_Data.csv"),
-  purpose: "assistants",
-});
-
-console.log("🪲  Creating assistant...");
-luxuryAssistant = await openai.beta.assistants.create({
-  name: "Luxury Apparel",
-  description: "Find or Analyze",
-  instructions: "You can search and analyze the luxury apparel CSV data.",
-  tools: [{ type: "code_interpreter" }],
-  model: "gpt-4-turbo-preview",
-  file_ids: [luxuryFile.id],
-});
-
-console.log("🪲  Creating thread...");
-const luxuryThread = await openai.beta.threads.create();
-
-// Run, Steps
+function debug(message) {
+  if (process.env.DEBUG) {
+    console.log(`🪲  ${message}`);
+  }
+}
 
 function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
@@ -60,7 +29,7 @@ function sleep(ms) {
 async function run(assistant, thread) {
   let run;
   let running = true;
-  console.log("🪲  Running...");
+  debug("Running...");
   run = await openai.beta.threads.runs.create(thread.id, {
     assistant_id: assistant.id,
   });
@@ -76,14 +45,59 @@ async function run(assistant, thread) {
     run.id
   );
   runSteps.data.forEach((step) => {
-    console.log("🪲  Step: " + JSON.stringify(step));
+    debug("Step: " + JSON.stringify(step));
   });
 }
+
+// Assistant
+
+const assistantsPage = await openai.beta.assistants.list({ limit: "100" });
+
+luxuryAssistant = assistantsPage.data.find((a) => a.name === "Luxury Apparel");
+
+if (luxuryAssistant !== undefined) {
+  debug(`Deleting assistant: ${luxuryAssistant.id}`);
+  await openai.beta.assistants.del(luxuryAssistant.id);
+}
+
+// File
+
+const fileObjectsPage = await openai.files.list();
+
+luxuryFile = fileObjectsPage.data.find(
+  (f) => f.filename === "Luxury_Products_Apparel_Data.csv"
+);
+
+if (luxuryFile !== undefined) {
+  debug(`Deleting file: ${luxuryFile.id}`);
+  await openai.files.del(luxuryFile.id);
+}
+
+// Assistant w/File, Thread, Message
+
+debug("Creating file...");
+luxuryFile = await openai.files.create({
+  file: fs.createReadStream("data/Luxury_Products_Apparel_Data.csv"),
+  purpose: "assistants",
+});
+
+debug("Creating assistant...");
+luxuryAssistant = await openai.beta.assistants.create({
+  name: "Luxury Apparel",
+  description: "Find or Analyze",
+  instructions: "You can search and analyze the luxury apparel CSV data.",
+  tools: [{ type: "code_interpreter" }],
+  model: "gpt-4-turbo-preview",
+  file_ids: [luxuryFile.id],
+});
+
+debug("Creating thread...");
+const luxuryThread = await openai.beta.threads.create();
 
 // Products Count
 
 const howManyProducts = "How many products do you have?";
-console.log(`💬 ${howManyProducts}`);
+log(howManyProducts);
 
 await openai.beta.threads.messages.create(luxuryThread.id, {
   role: "user",
@@ -92,12 +106,12 @@ await openai.beta.threads.messages.create(luxuryThread.id, {
 
 await run(luxuryAssistant, luxuryThread);
 messages = await openai.beta.threads.messages.list(luxuryThread.id);
-console.log(`🤖 ${messages.data[0].content[0].text.value}`);
+ai(messages.data[0].content[0].text.value);
 
 // Category Analysis
 
 const diagramQuery = "Show me a diagram of the categories.";
-console.log(`💬 ${diagramQuery}`);
+log(diagramQuery);
 await openai.beta.threads.messages.create(luxuryThread.id, {
   role: "user",
   content: diagramQuery,
@@ -108,17 +122,17 @@ messages = await openai.beta.threads.messages.list(luxuryThread.id);
 let fileID;
 for (const content of messages.data[0].content) {
   if (content.type === "text") {
-    console.log(`🤖 ${content.text.value}`);
+    ai(content.text.value);
   }
   if (content.type === "image_file") {
     fileID = content.image_file.file_id;
   }
 }
 if (fileID) {
-  console.log("🪲  Get file content:", fileID);
+  debug(`Get file content: ${fileID}`);
   const file = await openai.files.retrieve(fileID);
-  console.log("🪲  File: " + JSON.stringify(file));
-  console.log("🪲  Downloading file:", fileID);
+  debug(`File: ${JSON.stringify(file)}`);
+  debug(`Downloading file: ${fileID}`);
   const response = await openai.files.content(fileID);
   const writeStream = fs.createWriteStream("./images/diagram.png");
   response.body.pipe(writeStream);
@@ -126,4 +140,15 @@ if (fileID) {
 
 // Search Products
 
-// Find men's accessories for a sophisticated comic book enthusiast.
+const productSearch =
+  "Find men's accessories for a sophisticated comic book enthusiast.";
+log(productSearch);
+
+await openai.beta.threads.messages.create(luxuryThread.id, {
+  role: "user",
+  content: productSearch,
+});
+
+await run(luxuryAssistant, luxuryThread);
+messages = await openai.beta.threads.messages.list(luxuryThread.id);
+ai(messages.data[0].content[0].text.value);
